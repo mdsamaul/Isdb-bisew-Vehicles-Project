87c6ee19459ba0f953e848141de1e07231783991
mdsamaul <mdsamaul843@gmail.com>
2024-11-20 13:22:28 +0600

    dev idenity remove duplicate

--- Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Models/RideBook.cs
+++ Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Models/RideBook.cs
@@ -6,8 +6,6 @@ namespace Ride_Sharing_Project_isdb_bisew.Models
     public class RideBook
     {
         [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Primary Key
-=======
-        [Key , DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int BookingID { get; set; }
 
         [Required(ErrorMessage = "DriverVehicleID is required.")]
