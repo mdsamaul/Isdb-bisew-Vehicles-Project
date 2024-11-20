9297f767ed25579b02577e5c46b772b3e35ceebb
Rabbi Hassan <hassanrabbi409@gmail.com>
2024-11-20 11:05:16 -0800

    update

--- Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/obj/Debug/net8.0/Ride-Sharing-Project-isdb-bisew.AssemblyInfo.cs
+++ Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/obj/Debug/net8.0/Ride-Sharing-Project-isdb-bisew.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ride-Sharing-Project-isdb-bisew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2855da33a621e38e8a8fb520cd945beb0920d26b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f54ce80dc42f326ac950249925cb2a38b4c6df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ride-Sharing-Project-isdb-bisew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ride-Sharing-Project-isdb-bisew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
