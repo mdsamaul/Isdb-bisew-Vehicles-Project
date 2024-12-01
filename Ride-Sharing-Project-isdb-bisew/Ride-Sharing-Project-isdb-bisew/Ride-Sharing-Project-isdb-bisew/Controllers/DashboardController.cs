d575a4e99c6af275eb8e07998cc332e68abf7089
mdsamaul <mdsamaul843@gmail.com>
2024-12-02 02:06:08 +0600

    all most completeded dashbord design

---
+++ Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ride_Sharing_Project_isdb_bisew.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult WebsiteTraffic()
+        {
+            return PartialView("_WebsiteTraffic");  // Return the partial view
+        }
+
+        public IActionResult Settings()
+        {
+            return PartialView("_Settings");  // Return the partial view
+        }
+
+        public IActionResult Password()
+        {
+            return PartialView("_Password");  // Return the partial view
+        }
+    }
+}
