93878e097f95404806e8d5282f340b18b8e80a97
mdsamaul <mdsamaul843@gmail.com>
2024-12-02 11:05:37 +0600

    checking

--- Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/obj/Debug/net8.0/Ride-Sharing-Project-isdb-bisew.AssemblyInfo.cs
+++ Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/Ride-Sharing-Project-isdb-bisew/obj/Debug/net8.0/Ride-Sharing-Project-isdb-bisew.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ride-Sharing-Project-isdb-bisew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba6fd6903c9f7d4edac0daf12890179be8eb5b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4ca9f8cdc597066806d0714f86db3117220b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ride-Sharing-Project-isdb-bisew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ride-Sharing-Project-isdb-bisew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
